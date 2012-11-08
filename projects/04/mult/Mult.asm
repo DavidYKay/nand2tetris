b1447236c95b97f6091b19b9b2fbeff3cc8ab522
David Young-Chan Kay <davidykay@gmail.com>
2012-11-08 17:06:20 -0500

    Was so close but blew it.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,55 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+
+// value   = R0
+// counter = R1
+// output  = R2
+
+
+// let's not modify the original counter
+@1
+D = M
+@500 // Our own counter position
+M = D
+
+
+// load counter into our A register
+// if counter == 0:
+//   done!
+
+// @RAM[1]
+// @1 // -> no longer. now using our own counter
+(Loop)
+@500   // our counter
+D = M[1]
+@Done
+D;JEQ
+
+// else:
+//   add value to accumulator
+
+// @RAM[0]
+(Add)
+@0
+D = M     // Load multiplicand to D register
+
+// @RAM[2]
+@2
+M = M + D // Add the multiplicand to the outpu
+
+//   decrement counter
+// @RAM[1]
+// @1
+@500   // our counter
+M = M - 1
+
+//   repeat loop
+// JMP to begin
+@Loop
+0;JMP
+
+(Done)
+@Done
+0;JMP   // Infinite loop
