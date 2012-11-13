74ceefa6fc27a352e2cad876f14b92308c90bc26
David Young-Chan Kay <davidykay@gmail.com>
2012-11-13 15:15:24 -0500

    now have an infinite loop painting the screen

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,68 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+@LoopCounter
+
+(MainLoop)
+
+// If a key is pressed:
+//   D = BLACK
+// else:
+//   D = WHITE
+//@7
+// @11111
+// D = A
+//@paintColor  // Save the paintColor for later use
+//M = D
+
+@SCREEN        // = 16384
+D = A
+
+@paintPointer
+M = D
+
+// Screen is 512x256
+// divide by 16 == 32 x 16
+// 131072 pixels or 512 iterations through the loop
+
+
+// Paint the squares to the screen
+
+//@paintColor 
+//D = M
+
+(PaintLoop)
+@11111
+D = A
+
+@paintPointer
+A = M // load the paint location into A register
+M = D // paint the pixel to the color
+
+// Increment paintPointer
+//@16
+//D = A
+//@paintPointer
+//M = M + D
+@paintPointer
+M = M + 1
+
+// Compare paintPointer to maxPaintPosition
+
+@paintPointer
+D = M
+// D = @144756 - D  // Max paint position
+// D = @-144756 + D // Max paint position
+//@512
+@24576              // Debugger-derived last position
+//@16896            // Max Paint position
+D = D - A           // Max paint position
+
+// Repeat loop
+@PaintLoop
+// D;JGT
+D;JLT
+
+@MainLoop
+0;JMP   // Infinite loop
